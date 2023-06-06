135f7b62b5c19c36f4af4b76372879c2f3165499
Lora-Angelov <lora.angelovv@gmail.com>
2023-06-06 10:28:44 +0100

    true magic

--- build/actorsfront.js
+++ build/actorsfront.js
@@ -1,7 +1,7 @@
 // Fetch Actors Data
 async function fetchActorsData() {
     try {
-      const response = await fetch('https://tsiproject.azurewebsites.net/api/actors');
+      const response = await fetch('tsiprojectsql.mysql.database.azure.com/api/actors');
       if (!response.ok) {
         throw new Error('Error fetching actors data');
       }
